d58c95b9d4578cb0633d3e4e13419b0651d4b018
Cezary Wakuliński <cezary.wakulinski@conversion.pl>
2020-07-22 10:20:43 +0200

    Disable service worker

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'shards-ui/dist/css/shards.min.css'
 import 'Global.styl'
 
-import * as serviceWorker from './serviceWorker'
+//import * as serviceWorker from './serviceWorker'
 import styled from 'styled-components'
 
 import UTMGenerator from 'Container/UTM-Generator'
@@ -67,4 +67,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+//serviceWorker.unregister()
